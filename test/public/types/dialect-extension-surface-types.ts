7f485fefb9d35e0f6ffcd182d29a67c120cf42d4
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-26 16:31:41 -0700

    test(dialects): cover standard helper import boundary

--- test/public/types/dialect-extension-surface-types.ts
+++ test/public/types/dialect-extension-surface-types.ts
@@ -1,5 +1,5 @@
 import * as Schema from "effect/Schema"
-import { Column, Query, Table } from "effect-qb"
+import { Column, Function, Query, Scalar, Table } from "effect-qb"
 import * as My from "effect-qb/mysql"
 import * as Pg from "effect-qb/postgres"
 import * as Sq from "effect-qb/sqlite"
@@ -14,13 +14,43 @@ const events = Table.make("Events", {
 Pg.Renderer.make().render(Query.select({ id: events.id }).pipe(Query.from(events)))
 
 events.pipe(
-  Pg.Table.index({
-    columns: "id",
-    method: "btree"
-  })
+  Table.index("id").pipe(Pg.Table.using("btree"))
 )
 
 Pg.Schema.make("analytics").table("events", { id: Column.uuid() })
+
+Pg.Function.nextVal(Pg.sequence("events_id_seq"))
+
+// @ts-expect-error standard scalar APIs are exported from effect-qb
+type PgScalar = Pg.Scalar.Any
+
+// @ts-expect-error standard row-set APIs are exported from effect-qb
+type PgRowSet = Pg.RowSet.Any
+
+// @ts-expect-error standard function APIs are exported from effect-qb
+Pg.Function.lower(events.id)
+
+Function.lower(events.id)
+
+// @ts-expect-error MySQL has no dialect-specific function namespace
+My.Function.call("lower", events.id)
+
+// @ts-expect-error SQLite has no dialect-specific function namespace
+Sq.Function.call("lower", events.id)
+
+// @ts-expect-error standard query APIs are exported from effect-qb
+Pg.Query.select({ id: events.id })
+
+Query.select({ id: events.id })
+
+// @ts-expect-error MySQL query helpers are exported from effect-qb
+My.Query.select({ id: events.id })
+
+// @ts-expect-error SQLite query helpers are exported from effect-qb
+Sq.Query.select({ id: events.id })
+
+type ScalarAny = Scalar.Any
+void (null as unknown as ScalarAny)
 
 // @ts-expect-error portable tables are created from effect-qb/Table, not effect-qb/postgres
 Pg.Table.make("events", { id: Column.uuid() })
